999fb30b8658cc5c9746f8149c8ba8a0e8398a9e
Chyld Medford <chyld.medford@gmail.com>
2014-08-27 18:45:01 -0500

    updated debug

--- app/lib/debug.js
+++ app/lib/debug.js
@@ -3,20 +3,25 @@
 var chalk = require('chalk');
 
 exports.info = function(req, res, next){
-  console.log(chalk.bgRed('-------------------------------------------------------------'));
-  console.log(chalk.bold.red('TIME   :'), chalk.bold.blue(new Date()));
-  console.log(chalk.bold.red('PORT   :'), process.env.PORT);
-  console.log(chalk.bold.red('DB     :'), process.env.DB);
-  console.log(chalk.bold.red('URL    :'), req.url);
-  console.log(chalk.bold.red('VERB   :'), req.method);
-  console.log(chalk.bold.green('PARAMS :'), req.params);
-  console.log(chalk.bold.green('QUERY  :'), req.query);
-  console.log(chalk.bold.green('BODY   :'), req.body);
-  console.log(chalk.bold.green('LOCALS :'), res.locals);
-  console.log(chalk.bold.green('SESSION:'), req.session);
-  console.log(chalk.bold.red('SESSID :'), req.sessionID);
-  console.log(chalk.bold.red('HEADERS:'), req.headers.cookie);
-  console.log(chalk.bgRed('-------------------------------------------------------------'));
+  if(process.env.DEBUG){
+    console.log(chalk.bgRed('--------------------------------------------------------------------------------'));
+    console.log(chalk.bold.red('TIME   :'), chalk.bold.blue(new Date()));
+    console.log(chalk.bold.red('PORT   :'), process.env.PORT);
+    console.log(chalk.bold.red('DB     :'), process.env.DB);
+    console.log(chalk.bold.red('URL    :'), req.url);
+    console.log(chalk.bold.red('VERB   :'), req.method);
+    console.log(chalk.bold.green('PARAMS :'), req.params);
+    console.log(chalk.bold.green('QUERY  :'), req.query);
+    console.log(chalk.bold.green('BODY   :'), req.body);
+    console.log(chalk.bold.green('LOCALS :'), res.locals);
+    console.log(chalk.bold.green('SESSION:'), req.session);
+    console.log(chalk.bold.red('SESSID :'), req.sessionID);
+    console.log(chalk.bgBlue('--------------------------------------------------------------------------------'));
+    console.log(chalk.bold.red('HEADERS:'), req.headers);
+    console.log(chalk.bgBlue('--------------------------------------------------------------------------------'));
+    console.log(chalk.bold.red('ENV    :'), process.env);
+    console.log(chalk.bgRed('--------------------------------------------------------------------------------'));
+  }
 
   next();
 };
