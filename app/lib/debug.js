512b38b7f0f481c5055dee3648d135573bacf11d
Chyld Medford <chyld.medford@gmail.com>
2014-08-30 07:48:27 -0500

    debug work

--- app/lib/debug.js
+++ app/lib/debug.js
@@ -12,13 +12,14 @@ exports.info = function(req, res, next){
     console.log(chalk.bold.red('DB     :'), process.env.DB);
     console.log(chalk.bold.red('URL    :'), req.url);
     console.log(chalk.bold.red('VERB   :'), req.method);
+    console.log(chalk.bold.red('SESSID :'), req.sessionID);
     console.log(chalk.bold.green('PARAMS :'), req.params);
     console.log(chalk.bold.green('QUERY  :'), req.query);
     console.log(chalk.bold.green('BODY   :'), req.body);
     console.log(chalk.bold.green('USER   :'), req.user);
     console.log(chalk.bold.green('LOCALS :'), res.locals);
+    console.log(chalk.bold.green('FLASH  :'), req.session.flash);
     console.log(chalk.bold.green('SESSION:'), req.session);
-    console.log(chalk.bold.red('SESSID :'), req.sessionID);
 
     if(debug === 2){
       console.log(chalk.bgBlue('--------------------------------------------------------------------------------'));
